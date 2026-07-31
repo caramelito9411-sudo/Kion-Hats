5c4daa7b56e20b78592bfc4db7348b51fc47476d
mike <caramelito9411@gmail.com>
2026-07-31 11:56:14 -0600

    Corregir conexión Railway

--- database.py
+++ database.py
@@ -1,11 +1,22 @@
 import os
 import pymysql
+from urllib.parse import urlparse
 def get_connection():
+    database_url = os.getenv("MYSQL_URL")
+    if database_url:
+        url = urlparse(database_url)
+        return pymysql.connect(
+            host=url.hostname,
+            port=url.port,
+            user=url.username,
+            password=url.password,
+            database=url.path.lstrip("/"),
+            cursorclass=pymysql.cursors.DictCursor
+        )
     return pymysql.connect(
-        host=os.getenv("MYSQLHOST"),
-        user=os.getenv("MYSQLUSER"),
-        password=os.getenv("MYSQLPASSWORD"),
-        database=os.getenv("MYSQLDATABASE"),
-        port=int(os.getenv("MYSQLPORT")),
+        host="localhost",
+        user="root",
+        password="8410",
+        database="kion_hats",
         cursorclass=pymysql.cursors.DictCursor
     )
